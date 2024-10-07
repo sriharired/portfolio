90ffd015186b78d15df450bd2b044ad42769521d
sriharired <psrihari1952@gmail.com>
2024-10-07 14:43:27 -0400

    commit

--- src/Components/HeroSection.js
+++ src/Components/HeroSection.js
@@ -270,7 +270,7 @@ const HeroSection = () => {
         Innovative Solutions for a Digital World
       </FuturisticSubText>
       <NeonButton
-        href="https://drive.google.com/file/d/1n_zDDOudvvQ5CZHBlPOVXxLzxr5uH81D/view?usp=sharing"
+        href="https://drive.google.com/file/d/1AMlY6skdpI8DkT30lO2yhsimPm20Vkj3/view?usp=sharing"
         initial={{ opacity: 0, scale: 0.8 }}
         animate={{ opacity: 1, scale: 1 }}
         transition={{ delay: 1, duration: 0.5 }}
